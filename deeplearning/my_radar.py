e27edc7670746abe0c09ac5742198fc55ed4392f
xtc <466911416@qq.com>
2017-10-26 10:08:03 +0800

    new_radar

--- deeplearning/my_radar.py
+++ deeplearning/my_radar.py
@@ -1,20 +1,35 @@
 import tensorflow as tf
 import numpy as np
+import scipy.io as sio
+SNR = 10
+read_x_data = sio.loadmat('x_data.mat')  # 读取mat文件
+read_y_data = sio.loadmat('y_data.mat')
+# print(data.keys())   # 查看mat文件中的所有变量
+x1 = read_x_data['radar_in']
+x2 = read_y_data['y']
+x_data_true = np.append(x1,x1).reshape(1,-1)
+x_data_false = np.append(x1,x2).reshape(1,-1)
 
-xs = tf.placeholder(tf.float32,[None,100])
+Pnoise = pow(10,-SNR/10.)
+Noise = Pnoise * np.random.standard_normal(x_data_true.shape)
+x_data = np.zeros((100,2002))
+y_data = np.zeros((100,2))
+for i in range(50):
+    temp_x1 = x1*np.random.uniform(1,2)
+    temp_x2 = x2*np.random.uniform(1,2)
+    x_data[i*2] = np.append(x1,temp_x1)+Noise
+    x_data[i*2+1] = np.append(x1,temp_x2)+Noise
+    y_data[i*2] = [0,1.]
+    y_data[i*2+1] = [1.,0]
+
+
+
+xs = tf.placeholder(tf.float32,[None,2002])
 ys = tf.placeholder(tf.float32,[None,2]) #输出为真假的概率
-x_data = np.array([np.random.normal(1,0.1,[100]) for i in range(1000)])
-y_data = np.zeros((1000,2))
-for i in range(1000):
-    k = np.random.randint(0,2)
-    y_data[i,0]=(k+1)%2
-    y_data[i,1]=k%2
-x_test = np.array([np.random.normal(1,0.1,[100]) for i in range(1000)])
-y_test = np.zeros((1000,2))
-for i in range(1000):
-    k = np.random.randint(0,2)
-    y_test[i,0]=(k+1)%2
-    y_test[i,1]=k%2
+
+x_test = x_data
+y_test = y_data
+
 def add_layer(inputs,in_size,out_size,activation_function=None):
     Weights = tf.Variable(tf.truncated_normal([in_size,out_size],stddev=0.1))
     biases = tf.Variable(tf.zeros([1,out_size])+0.1)
@@ -25,8 +40,10 @@ def add_layer(inputs,in_size,out_size,activation_function=None):
         outputs = activation_function(Wx_plus_b)
     return outputs
 
-pre_1 = add_layer(xs,100,500,activation_function=tf.nn.relu)
-prediction = add_layer(pre_1,500,2,activation_function=tf.nn.softmax)
+pre_1 = add_layer(xs,2002,8000,activation_function=tf.nn.relu)
+pre_2 = add_layer(pre_1,8000,4000,activation_function=tf.nn.sigmoid)
+pre_3 = add_layer(pre_2,4000,500,activation_function=tf.nn.relu)
+prediction = add_layer(pre_3,500,2,activation_function=tf.nn.softmax)
 
 loss = tf.reduce_mean(-tf.reduce_sum(ys*tf.log(prediction)))
 train_step = tf.train.GradientDescentOptimizer(1e-4).minimize(loss)
@@ -36,6 +53,7 @@ sess.run(tf.global_variables_initializer())
 
 for i in range(1000):
     sess.run(train_step,feed_dict={xs:x_data,ys:y_data})
+    print(i)
     if i % 50 == 0:
         sess.run(loss,feed_dict={xs:x_test,ys:y_test})
         print(sess.run(prediction,feed_dict={xs:x_test,ys:y_test}))
